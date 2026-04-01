283fe7ec85107031331a6235d65cf152d226264d
Sharmin Khanan <skjerin2020@gmail.com>
2026-04-01 15:34:09 +0600

    cart updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -21,12 +21,12 @@ export default function Cart ({carts, setCarts}){
                  <div className="space-y-4">
                 {
                 carts.map((item) => <div className="flex justify-between items-center rounded-lg  bg-gray-200" key={item.id}>
-                    <div className="p-4">
-                        <span className="flex gap-2  ">
-                         <img src={item.icon} alt="icon"  className="mx-auto object-contain text-black"/>
-                         {item.name}
+                    <div className="p-4 ">
+                        <span className="flex gap-4">
+                         <img src={item.icon} alt="icon"  className="mx-auto object-contain text-black bg-white p-3 rounded-full w-20 h-20 mt-4"/>
+                       <h2 className="text-xl font-bold mt-10">{item.name}</h2>
                         </span>
-                        <p className="font-semibold text-2xl ml-10 text-black">${item.price}</p>
+                        <p className="font-semibold text-3xl ml-24 text-black ">${item.price}</p>
                          
                     </div>
                     <div>
@@ -44,8 +44,8 @@ export default function Cart ({carts, setCarts}){
             }
             </div>
             <div className="flex justify-between bg-base-200 text-gray-500 mt-3 p-4 rounded-xl "> 
-                <div>Total:</div>
-                <div>{totalPrice}</div>
+                <div className="text-xl font-bold">Total:</div>
+                <div className="text-xl font-bold">{totalPrice}</div>
             </div>
             <button onClick={handlePayment} className="btn w-full rounded-3xl text-white bg-red-600 mt-4">
                 Procued to checkout</button></>
