41dec034a4b11c8af1262746f0f491acec7b1fa3
Sharmin Khanan <skjerin2020@gmail.com>
2026-04-01 18:29:39 +0600

    card updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,26 +25,26 @@ function Carts ({product, carts, setCarts}) {
                         <div 
                                 key={product.id}
                                 className="p-8 ">
-                                    
-                                    
-                                    <span className="flex items-center justify-between w-20 h-20 border  rounded-full">
+                                    <div className="flex">
+                                          <span className="flex items-center justify-between w-20 h-20 border  rounded-full">
                                        <img src={product.icon} alt="icon"  className="mx-auto"/>
+                                        
                                         </span>
+                                        <div className="badge badge-warning ml-60">{product.tagType}</div>
+                                      
+                                    </div>
+                                  
                                         <div className="space-y-3 mt-4">
                                              <h2 className="font-bold text-2xl">{product.name}</h2>
-                                    
                                     <p className="font-semibold">{product.description}</p>
                                     
-                                      <div>${product.price}</div>
-                                   <h3 className="flex gap-2 items-center"> 
-                                    <FaCheck className="text-green-500 w-5 h-5 "/>{product.tag}</h3>
-                                    
-                                    <h3 className="flex gap-2 items-center">
-                                        <FaCheck className="text-green-500 w-5 h-5 "/>{product.tagType}</h3>
-                                    <h3 className="flex gap-2 items-center">
-                                        <FaCheck className="text-green-500 w-5 h-5 "/>{product.period}</h3>
+                                      <div>${product.price}/{product.period}</div>
                                     <h3 className="flex gap-2 items-center">
                                         <FaCheck className="text-green-500 w-5 h-5 "/>{product.features}</h3>
+                                         <h3 className="flex gap-2 items-center">
+                                        <FaCheck className="text-green-500 w-5 h-5 "/>{product.secondcontent}</h3>
+                                         <h3 className="flex gap-2 items-center">
+                                        <FaCheck className="text-green-500 w-5 h-5 "/>{product.thirdcontent}</h3>
                                         </div>
                                     
                                     </div>
