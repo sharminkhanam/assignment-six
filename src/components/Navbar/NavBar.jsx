8d360a3c562b2cca30625788fa0c8ed4155507df
Sharmin Khanan <skjerin2020@gmail.com>
2026-04-01 15:16:10 +0600

    navbar updated

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -2,28 +2,28 @@ import { CiShoppingCart } from "react-icons/ci";
 
 function NavBar ({carts,}) {
   return (
-    <div className="navbar container mx-auto shadow p-4">
+    <div className="navbar container mx-auto bb-base-100 shadow p-4">
       <div className="navbar-start">
         <div className="flex items-center">
-             <h2>DigiTools</h2>
+             <h2 className="font-bold text-2xl bg-gradient-to-r from-blue-500 to-purple-800 bg-clip-text  text-transparent">DigiTools</h2>
         </div>
        
       </div>
-      <div className="navbar-center">
+      <div className="navbar-center hidden md:flex">
         <ul className="menu menu-horizontal">
           <li>
-            <a href="">Products</a>
+            <a href="" className="text-xl text-black">Products</a>
           </li>
           <li>
-            <a href="">Features</a>
+            <a href="" className="text-xl text-black">Features</a>
           </li>
           <li>
-            <a href="">Pricing</a>
+            <a href="" className="text-xl text-black">Pricing</a>
           </li>
           <li>
-            <a href="">Terminals</a>
+            <a href="" className="text-xl text-black">Terminals</a>
           </li>
-          <li><a href="">FAQ</a></li>
+          <li><a href="" className="text-xl text-black">FAQ</a></li>
         </ul>
       </div>
       <div className="navbar-end space-x-3">
@@ -32,9 +32,9 @@ function NavBar ({carts,}) {
         <CiShoppingCart className="w-8 h-8"/>
         
       </div>
-      <button>Login</button>
+      <button className="text-xl text-black">Login</button>
       <div>
-        <button className="btn btn-primary rounded-full ">Get Started</button>
+        <button className="btn bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-full ">Get Started</button>
       </div>
       </div>
      
